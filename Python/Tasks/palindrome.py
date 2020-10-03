dd359d223d6e44cbbb09de959a423114d66ca600
ShGanesh <54804789+ShGanesh@users.noreply.github.com>
2020-10-03 07:42:57 +0530

    Update palindrome.py
    Separated number into str in tuple.

--- Python/Tasks/palindrome.py
+++ Python/Tasks/palindrome.py
@@ -1,12 +1,15 @@
 def palindrome(n):
-  m=n 
-  a=0
-  while(m!=0):
-    
-    
-  if (n == a):
-    return "it is a palindrome number"
-  else:
-    return "it is not palindrome number"
+    m = n                                   
+    while (n != 0):                         
+        tpl = tuple(str(n))                 
+        break                               
+    a = int("".join(tpl[::-1]))             
+                                            
+    if (m == a):                            
+        return "it is a palindrome number"  
+    else:                                   
+        return "it is not palindrome number"            
+                                        
+
     
 print palindrome(156)
