acbcd480a478cd646bd6644a700e05fb36b30180
ShGanesh <54804789+ShGanesh@users.noreply.github.com>
2020-10-03 08:35:25 +0530

    Update removing_duplicate_values.py

--- Python/Tasks/removing_duplicate_values.py
+++ Python/Tasks/removing_duplicate_values.py
@@ -4,13 +4,13 @@ write a program to print this list after removing all duplicate
 values with original order reserved.
 """
 
-def removeDuplicate( lists):
-    newlist=[]
-    seen = set()
-    for item in lists:
-        
 
-    return newlist
+def removeDuplicate( lists):
+    newlist=[]                                      
+    for item in lists:                              
+        if item not in newlist: newlist.append(item)
+    return newlist                                  
+
 
 li=[12,24,35,24,88,120,155,88,120,155]
 print removeDuplicate(li)
